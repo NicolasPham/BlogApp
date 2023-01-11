41409c34eb0727726672c5709881752c576ad145
Nicolas Pham <phd.ngoc163@gmail.com>
2023-01-11 12:51:05 +0000

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import "../styles.scss";
+import { Link } from 'react-router-dom';
+import Logo from '../assets/logo.png'
+
+const cats = ["ART", "CINEMA", "SPORTS", "TECHNOLOGY", "DESIGN", "FOOD"]
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex-row-center'>
+      <div className='logo'><img src={Logo} alt="" /></div>
+      <div className='content flex-row-center'>
+
+        <div className='cats flex-row-center'>
+          {cats.map((cat, index) => (
+            <Link key={index} to={`/?cat=${cat}`} className='link'><span >
+              {cat}</span></Link>
+          ))}
+        </div>
+        <span className='write'>WRITE</span>
+        <div className='user flex-row-center'>
+          <span>Nick</span>
+          <span>LOGOUT</span>
+        </div>
+      </div>
+
+    </nav>
   )
 }
 
